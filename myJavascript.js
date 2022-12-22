ebb1ba28d7e3674619104a3a741609fc761f313c
Diego <diegoyegrosa@gmail.com>
2022-12-22 09:53:28 -0300

    Ejercicios de Javascript

---
+++ myJavascript.js
@@ -0,0 +1,37 @@
+// 1. Escribir una función en JavaScript que calcule la versión que suma en la ecuación
+// cuadrática
+//(-b + sqrt(b^2 - 4ac))/2a
+
+// var sumaEcuacionCuadratica = (a, b, c) => {
+//     console.log((-b + Math.sqrt((Math.pow(b, 2)) - 4 * a * c)) / 2 * a)
+// }
+
+// sumaEcuacionCuadratica(1, 2, -8)
+
+// 2. Escribir una función en JavaScript que calcule la versión que resta en la ecuación
+// cuadrática
+// (-b - sqrt(b^2 - 4ac))/2a
+// var restaEcuacionCuadratica = (a, b, c) => {
+//     console.log((-b - Math.sqrt((Math.pow(b, 2)) - 4 * a * c)) / 2 * a)
+// }
+
+// restaEcuacionCuadratica(1, 2, -8)
+
+// 3. Escribir una función en JavaScript que reciba un id y un string y ponga como innerHTML
+// del elemento con el id referenciado el valor del string
+
+// var cuestionDelEjercicio = (id, string) => {
+//     let myThing = document.getElementById(id).innerHTML = string
+//     console.log(myThing)
+// }
+// cuestionDelEjercicio("idOficial", "HOLA")
+
+
+// Escribir una función en JavaScript que reciba un id, y ponga en el innerHTML del
+// elemento con el id referenciado un número aleatorio entre 1 y 100
+
+// var cuestionDelEjercicio = (id) => {
+//     let myThing = document.getElementById(id).innerHTML = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+//     console.log(myThing)
+// }
+// cuestionDelEjercicio("idOficial")
